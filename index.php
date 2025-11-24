1ed4eab556eb39c5eb053e8c71206a0edaab0188
neil rona <jonaran669@gmail.com>
2025-11-24 17:27:51 +0800

    version2

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>PHP + Tailwind Example</title>
     <link href="dist/output.css" rel="stylesheet">
 </head>
-<body class="bg-gray-100 font-sans">
+<body class="bg-gray-200 font-sans">
 
     <?php
         $username = "Web Developer";
